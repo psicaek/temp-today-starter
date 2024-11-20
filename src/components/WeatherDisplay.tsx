5d2943e1086f8324636e93174294be33d1cdeff8
psychogyiosioannis <ioannis.psychogyios@abl.de>
2024-11-20 09:43:51 +0100

    Exercise 4: Refactor WeatherDisplay to use styled-components

--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
 
 type WeatherDisplayProps = {
     temperature: number;
@@ -7,28 +8,45 @@ type WeatherDisplayProps = {
 
 export const WeatherDisplay: React.FC<WeatherDisplayProps> = ({temperature, condition})  =>{
 return (
-    <View style={styles.container}>
-    <Text style={styles.temperature}>{temperature}°C</Text>
-    <Text style={styles.condition}> {condition} </Text>
-    </View>
+    <Container>
+    <Temperature >{temperature}°C</Temperature>
+    <Condition> {condition} </Condition>
+    </Container>
 )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        alignItems:"center",
-        marginTop: 50,
-    },
-    temperature: {  
-        fontSize: 48,
-        color: "#333",
-        fontWeight: "bold"
-    },
-    condition: {
-        color: "#666",
-        fontSize: 24,
-        marginTop: 10,
+const Container =  styled.View`
+        alignItems: center;
+        marginTop: 50;
+`;
+
+const Temperature =  styled.Text`
+        fontSize: 48;
+        color: #333;
+        fontWeight: bold;
+`;
+
+const Condition =  styled.Text`
+        fontSize: 24;
+        color: #666;
+        marginTop: 10;
+`;
+
+//const styles = StyleSheet.create({
+  //  container: {
+    //    alignItems:"center",
+      //  marginTop: 50,
+    //},
+    //temperature: {  
+     //   fontSize: 48,
+      //  color: "#333",
+      //  fontWeight: "bold"
+   // },
+    //condition: {
+     //   color: "#666",
+      //  fontSize: 24,
+       // marginTop: 10,
         
-      },
-  });
+     // },
+  //});
   export default WeatherDisplay; 
